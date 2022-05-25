5d8651662e1376dd014df33785d41092d8102e8e
Sagina1 <mwimayvonne@gmail.com>
2022-05-25 21:10:24 +0300

    assignment on while loops

--- Assignment6.py
+++ Assignment6.py
@@ -1,1 +1,47 @@
-colours = ("blue","green","orange","purple","violet","red")
+#!usr/bin/python
+#while loops
+#Crunchy biscuit
+#25th May 2022
+
+#making each colour appear in caps
+colours = ("blue","green","orange","purple","violet","yellow")
+i=0
+while i < len(colours):
+    if colours[0] == "blue":
+     print(colours[0].upper())
+     i += 1
+
+
+i=1
+while i< len(colours):
+    if colours[1]=="green":
+        print(colours[1].upper())
+        i += 1
+
+
+i=2
+while i < len(colours):
+    if colours[2]== "orange":
+        print(colours[2].upper())
+        i += 1        
+
+
+i=3
+while i< len(colours):
+    if colours[3]=="purple":
+        print(colours[3].upper())
+        i += 1        
+
+
+i=4
+while i < len(colours):
+    if colours[4]=="violet":
+        print(colours[4].upper())
+        i += 1
+
+
+i=5
+while i < len(colours):
+    if (colours[5]=="yellow"):
+        print(colours[5].upper())
+        i += 1        
